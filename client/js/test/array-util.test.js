9646819f215f59d9bba57038951e75458783b767
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-04-24 17:43:34 -0400

    Add Jest unit test for getLetterRange()

--- client/js/test/array-util.test.js
+++ client/js/test/array-util.test.js
@@ -1,4 +1,5 @@
-const { getRange } = require('../array-util');
+const { getRange,
+        getLetterRange, } = require('../array-util');
 
 describe('array-util', () => {
 
@@ -13,6 +14,20 @@ describe('array-util', () => {
 
     it('produces a valid negative range', () => {
       expect(getRange(-10, -7)).toEqual([-10, -9, -8, -7]);
+    });
+  });
+
+  describe('getLetterRange()', () => {
+    it('produces a valid single letter range', () => {
+      expect(getLetterRange('Q', 1)).toEqual(['Q']);
+    });
+
+    it('produces a valid letter range starting at A', () => {
+      expect(getLetterRange('A', 5)).toEqual(['A', 'B', 'C', 'D', 'E']);
+    });
+
+    it('produces a valid letter range starting at B', () => {
+      expect(getLetterRange('B', 5)).toEqual(['B', 'C', 'D', 'E', 'F']);
     });
   });
 });
