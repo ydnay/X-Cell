e49c35713e4b74a5a84809a7bc4b651745f9568c
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-04-24 17:34:23 -0400

    Add utility function getLetterRange

--- client/js/array-util.js
+++ client/js/array-util.js
@@ -3,6 +3,14 @@ const getRange = function (fromNum, toNum) {
     (unused, i) => i + fromNum);
 };
 
+const getLetterRange = function (firstLetter = 'A', numLetters) {
+  const rangeStart = firstLetter.charCodeAt(0);
+  const rangeEnd = rangeStart + numLetters - 1;
+  return getRange(rangeStart, rangeEnd)
+    .map(charCode => String.fromCharCode(charCode));
+};
+
 module.exports = {
   getRange: getRange,
+  getLetterRange: getLetterRange,
 };
