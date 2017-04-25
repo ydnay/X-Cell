fbfb34eafa64d83225a85f3c8eadc5afe081c3f7
Yandy Gonzalez <yandygonzalez@gmail.com>
2017-04-25 16:57:21 -0400

    Add data rows unit test

--- client/js/test/table-view.test.js
+++ client/js/test/table-view.test.js
@@ -11,6 +11,32 @@ describe('table-view', () => {
     document.documentElement.innerHTML = html;
   });
 
+  describe('table body', () => {
+    it('has the right size', () => {
+      // set up the initial state
+      const numCols = 6;
+      const numRows = 10;
+      const model = new TableModel(numCols, numRows);
+      const view = new TableView(model);
+      view.init();
+
+      // inspect the initial state
+      let ths = document.querySelectorAll('THEAD TH');
+      expect(ths.length).toBe(numCols);
+    });
+    it('fills in values from the model', () => {
+      // set up the initial state
+      const model = new TableModel(3, 3);
+      const view = new TableView(model);
+      model.setValue({ col: 2, row: 1 }, '123');
+      view.init();
+
+      // inspect the initial state
+      const trs = document.querySelectorAll('TBODY TR');
+      expect(trs[1].cells[2].textContent).toBe('123');
+    });
+  });
+
   describe('table header', () => {
     it('has valid column header lables', () => {
       // set up the initial state
